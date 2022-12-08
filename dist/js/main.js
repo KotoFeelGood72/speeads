459d06e4c91bb1b3981046ae6797356005861f49
KotoFeelGood72 <kotenkolove@yandex.ru>
2022-12-08 11:36:41 +0300

    Complete all

--- dist/js/main.js
+++ dist/js/main.js
@@ -146,27 +146,27 @@ console.info('%c%s', styles, message);
 
 
 
-$(document).ready(function() {
-	const btns = document.querySelectorAll('.btn')
+// $(document).ready(function() {
+// 	const btns = document.querySelectorAll('.btn')
 
-	btns.forEach(el => {
-			el.addEventListener('click', function(e) {
-					let
-							size = Math.max(this.offsetWidth, this.offsetHeight),
-							x = e.offsetX - size / 2,
-							y = e.offsetY - size / 2,
-							wave = this.querySelector('.wave')
+// 	btns.forEach(el => {
+// 			el.addEventListener('click', function(e) {
+// 					let
+// 							size = Math.max(this.offsetWidth, this.offsetHeight),
+// 							x = e.offsetX - size / 2,
+// 							y = e.offsetY - size / 2,
+// 							wave = this.querySelector('.wave')
 	
-					// Create an element if it doesn't exist
-					if (!wave) {
-							wave = document.createElement('span')
-							wave.className = 'wave'
-					}
-					wave.style.cssText = `width:${size}px;height:${size}px;top:${y}px;left:${x}px`
-					this.appendChild(wave)
-			})
-	})
-})
+// 					// Create an element if it doesn't exist
+// 					if (!wave) {
+// 							wave = document.createElement('span')
+// 							wave.className = 'wave'
+// 					}
+// 					wave.style.cssText = `width:${size}px;height:${size}px;top:${y}px;left:${x}px`
+// 					this.appendChild(wave)
+// 			})
+// 	})
+// })
 
 
 
